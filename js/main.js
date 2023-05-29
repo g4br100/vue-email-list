a293fded9395eede15766f12e08d28dc77ba5baf
user1 <user@email.com>
2023-05-29 16:43:06 +0200

    Completato

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var { createApp } = Vue
+createApp({
+    data(){
+        return{
+            emails: [],
+        }
+    },
+    mounted(){
+        for (let i = 0 ; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(risposta => {
+                    this.emails.push(risposta.data.response);
+                })
+        } 
+    }  
+}).mount('#app')
